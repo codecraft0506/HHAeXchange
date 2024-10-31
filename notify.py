1a457a85b790507b676afca90e022ef307176c5a
Jianrong0830 <ryanchen0830b@gmail.com>
2024-10-31 20:33:53 +0800

    send image

--- notify.py
+++ notify.py
@@ -5,11 +5,12 @@ load_dotenv()
 
 DC_URL = os.environ.get('DC_URL')
 
-def send_notification(message, user=''):
+def send_notification(message, user='', image_path=None):
     payload = {
         'content': f"{user} {message}",
     }
-    requests.post(DC_URL, json=payload)
+    files = {'file': open(image_path, 'rb')} if image_path else None
+    requests.post(DC_URL, data=payload, files=files)
 
 if __name__ == '__main__':
-    send_notification('DC Notify Test', 'Ryan')
+    send_notification('DC Notify Test', 'Ryan', 'test_img.jpg')
